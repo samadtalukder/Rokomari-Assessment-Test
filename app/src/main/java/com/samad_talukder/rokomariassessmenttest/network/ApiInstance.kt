f3819292a33022e9f5a0822b002d6580fa41da2f
samadtalukder <samadtalukdar6@gmail.com>
2021-08-23 21:29:08 +0600

    create api instance

---
+++ app/src/main/java/com/samad_talukder/rokomariassessmenttest/network/ApiInstance.kt
@@ -0,0 +1,22 @@
+package com.samad_talukder.rokomariassessmenttest.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiInstance {
+    val api: ApiInterface by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder().addInterceptor(logging).build()
+
+        Retrofit.Builder()
+            .baseUrl(ApiUrl.API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(ApiInterface::class.java)
+    }
+}
