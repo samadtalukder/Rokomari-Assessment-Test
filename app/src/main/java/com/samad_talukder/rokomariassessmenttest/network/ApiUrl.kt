fc1c0bb4c30c126e20bbe931f87c80eda48b86c9
samadtalukder <samadtalukdar6@gmail.com>
2021-08-23 21:13:05 +0600

    add all api url

---
+++ app/src/main/java/com/samad_talukder/rokomariassessmenttest/network/ApiUrl.kt
@@ -0,0 +1,14 @@
+package com.samad_talukder.rokomariassessmenttest.network
+
+object ApiUrl {
+    const val API_BASE_URL = "http://202.84.44.253:5005/api/v1/"
+
+    const val API_SIGN_UP = "auth/register/"
+    const val API_LOG_IN = "auth/token/"
+    const val API_BOOK_LIST = "books/"
+    const val API_BOOK_DETAILS = "books/"
+    const val API_PURCHASE = "purchase/"
+    const val API_MY_WALLET = "wallet/"
+
+
+}
