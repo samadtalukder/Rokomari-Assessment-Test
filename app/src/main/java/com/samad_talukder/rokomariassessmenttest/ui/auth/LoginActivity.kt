f4e9dba4e8e336bec6d98cd479e78e0c68e8d7d8
samadtalukder <samadtalukdar6@gmail.com>
2021-08-23 21:03:26 +0600

    login view create

---
+++ app/src/main/java/com/samad_talukder/rokomariassessmenttest/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.samad_talukder.rokomariassessmenttest.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.samad_talukder.rokomariassessmenttest.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
