896bb9354f2a21b92b459123470f4f70e0dab3b3
samadtalukder <samadtalukdar6@gmail.com>
2021-08-24 17:52:21 +0600

    repository api add

--- app/src/main/java/com/samad_talukder/rokomariassessmenttest/repository/RokomariRepository.kt
+++ app/src/main/java/com/samad_talukder/rokomariassessmenttest/repository/RokomariRepository.kt
@@ -1,5 +1,6 @@
 package com.samad_talukder.rokomariassessmenttest.repository
 
+import com.samad_talukder.rokomariassessmenttest.model.request.PurchaseBookRequest
 import com.samad_talukder.rokomariassessmenttest.model.request.SignInRequest
 import com.samad_talukder.rokomariassessmenttest.model.request.SignUpRequest
 import com.samad_talukder.rokomariassessmenttest.network.ApiInstance
@@ -7,16 +8,25 @@ import com.samad_talukder.rokomariassessmenttest.network.ApiInstance
 class RokomariRepository {
 
     suspend fun signUpRequest(signUpRequest: SignUpRequest) =
-        ApiInstance.api.signUpRequest(signUpRequest)
+        ApiInstance.api.callSignUpApi(signUpRequest)
 
     suspend fun signInRequest(signInRequest: SignInRequest) =
-        ApiInstance.api.sigInRequest(signInRequest)
+        ApiInstance.api.callSigInApi(signInRequest)
 
     suspend fun allBookRequest(token: String, pageNo: Int, limit: Int, isNewArrival: String) =
-        ApiInstance.api.getNewArrivalBook(token, pageNo, limit, isNewArrival)
+        ApiInstance.api.callNewArrivalBookApi(token, pageNo, limit, isNewArrival)
 
     suspend fun bookDetails(token: String, bookId: String) =
-        ApiInstance.api.getBookDetails(token, bookId)
+        ApiInstance.api.callBookDetailsApi(token, bookId)
+
+    suspend fun bookPurchase(token: String, purchaseBookRequest: PurchaseBookRequest) =
+        ApiInstance.api.callPurchaseApi(token, purchaseBookRequest)
+
+    suspend fun bookPurchaseByUser(token: String) =
+        ApiInstance.api.callPurchaseByUserBookApi(token)
+
+    suspend fun myWallet(token: String) =
+        ApiInstance.api.callPurchaseByUserBookApi(token)
 
 
 }
