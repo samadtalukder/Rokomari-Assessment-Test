fbeb9b1d66b413eb4d1d9560de575d4be2587900
samadtalukder <samadtalukdar6@gmail.com>
2021-08-26 01:23:59 +0600

    Common utils function add

---
+++ app/src/main/java/com/samad_talukder/rokomariassessmenttest/utils/CommonUtils.kt
@@ -0,0 +1,44 @@
+package com.samad_talukder.rokomariassessmenttest.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import com.bumptech.glide.Glide
+
+fun View.visible(isVisible: Boolean) {
+    visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+fun ImageView.loadImage(imgUrl: String) {
+    Glide.with(this).load(imgUrl).into(this)
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun isOnline(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val capabilities =
+        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    if (capabilities != null) {
+        when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            }
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            }
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+    }
+    return false
+}
